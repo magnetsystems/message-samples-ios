67c5ed0cf93f0ec9f031f8dba5b15c17307a4ec6
Jason Ferguson <jason.ferguson@magnet.com>
2015-07-20 17:21:46 -0700

    Adding push notification sample code to both apps

--- RPSLS/RPSLS/AppDelegate.m
+++ RPSLS/RPSLS/AppDelegate.m
@@ -32,8 +32,55 @@
 	NSString *pathAndFileName = [[NSBundle mainBundle] pathForResource:@"Configurations" ofType:@"plist"];
 	NSAssert([[NSFileManager defaultManager] fileExistsAtPath:pathAndFileName], @"You must include your Configurations.plist file in the project. You can download this file on the Settings page of the Magnet Message Web Interface");
 
+	/* //UNCOMMENT THIS SECTION FOR PUSH NOTIFICATIONS
+	 //You need to change the bundle Identifier to match the one your push certificate is set up to work with.
+	 
+	 //Code to register for notifications
+	 if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+		UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeBadge
+	 |UIUserNotificationTypeSound
+	 |UIUserNotificationTypeAlert) categories:nil];
+		[application registerUserNotificationSettings:settings];
+	 } else {
+		UIRemoteNotificationType myTypes = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
+		[application registerForRemoteNotificationTypes:myTypes];
+	 }
+	 */
 	return YES;
 }
+
+/* //UNCOMMENT THIS SECTION FOR PUSH NOTIFICATIONS
+ - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+	[[MMXClient sharedClient] updateRemoteNotificationDeviceToken:deviceToken];
+ }
+ 
+ - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+	NSLog(@"didFailToRegisterForRemoteNotificationsWithError = %@",error.localizedDescription);
+ }
+ 
+ - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+ {
+	//register to receive notifications
+	[application registerForRemoteNotifications];
+ }
+ 
+ - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+ 
+	//In the case of a silent notification use the following code to see if it is a wakeup notification
+	if ([MMXRemoteNotification isWakeupRemoteNotification:userInfo]) {
+ //Send local notification to the user or connect via MMXClient
+ completionHandler(UIBackgroundFetchResultNewData);
+	} else if ([MMXRemoteNotification isMMXRemoteNotification:userInfo]) {
+ NSLog(@"userInfo = %@",userInfo);
+ //Check if the message is designed to wake up the client
+ [MMXRemoteNotification acknowledgeRemoteNotification:userInfo completion:^(BOOL success) {
+ completionHandler(UIBackgroundFetchResultNewData);
+ }];
+	} else {
+ completionHandler(UIBackgroundFetchResultNoData);
+	}
+ }
+ */
 
 - (void)applicationWillResignActive:(UIApplication *)application {
 	[[MMXClient sharedClient] disconnect];
