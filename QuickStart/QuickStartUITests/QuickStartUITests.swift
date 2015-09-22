98cd4dcff51a877baf67882f506fa70aed56ba1a
Daniel Gulko <dgulko@DanielGulko-macpro.local>
2015-09-22 16:59:36 -0700

    commented out notification alert in app delegate and added sleep in quicstart ui test

--- QuickStart/QuickStartUITests/QuickStartUITests.swift
+++ QuickStart/QuickStartUITests/QuickStartUITests.swift
@@ -18,9 +18,15 @@ class UITestQuickStart: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        continueAfterFailure = true
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+        sleep(5)
+        print("done launching")
+        
+        if XCUIApplication().alerts.collectionViews.buttons["OK"].exists {
+            XCUIApplication().alerts.collectionViews.buttons["OK"].tap()
+        }
         
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -30,15 +36,14 @@ class UITestQuickStart: XCTestCase {
     }
     
     func launchClearScreen() {
-        
-        app.alerts["Logged In"].collectionViews.buttons["OK"].tap()
+        print("running clear screen function");
         app.toolbars.containingType(.Button, identifier:"Send").childrenMatchingType(.TextView).element.pressForDuration(1.0)
         app.menuItems["Select All"].tap()
         app.menuItems["Cut"].tap()
     }
     
     func test1SendMessageQuickstart() {
-        
+        print("running first test")
         launchClearScreen()
         XCTAssert(app.buttons["Send"].exists)
         app.navigationBars["MessagesView"].buttons["Share"].tap()
