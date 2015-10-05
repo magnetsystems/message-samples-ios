aa889eec5ff9f58b77a3c19e3745c57b329bcc1e
Daniel Gulko <dgulko@DanielGulko-macpro.local>
2015-10-05 16:10:57 -0700

    added sign in tests rpsls, added keyboard dismiss for SignInViewController and fixed spelling for error

---
+++ RPSLS/RPSLSUITests/RPSLSSignInTests.swift
@@ -0,0 +1,120 @@
+//
+//  RPSLSUITests.swift
+//  RPSLSUITests
+//
+//  Created by Daniel Gulko on 10/5/15.
+//  Copyright © 2015 Magnet Systems, Inc. All rights reserved.
+//
+
+import XCTest
+
+class RPSLSUITests: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUp() {
+        super.setUp()
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        app.launch()
+    
+        if XCUIApplication().alerts.collectionViews.buttons["OK"].exists {
+            app.alerts.collectionViews.buttons["OK"].tap()
+        }
+
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    // sign in
+    private func signIn(user:String, password:String) {
+        let usernameTextField = app.textFields["Username"]
+        usernameTextField.tap()
+        usernameTextField.typeText(user)
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText(password)
+    }
+    
+    // wait for element
+    private func evaluateElementExist(element:AnyObject) {
+        let exists = NSPredicate(format: "exists == 1")
+        expectationForPredicate(exists, evaluatedWithObject: element, handler: nil)
+        waitForExpectationsWithTimeout(30, handler: nil)
+    }
+    
+    // confirm alert
+    private func confirmAlert(title: String, message: String) {
+        if app.alerts.collectionViews.buttons["OK"].exists {
+            let title = app.staticTexts[title]
+            let message = app.staticTexts[message]
+            
+            evaluateElementExist(title)
+            evaluateElementExist(message)
+            app.buttons["OK"].tap()
+        }
+    }
+    
+    // sign in and registration tests
+    func test1signInNonExistingUser() {
+        let signinButton = app.buttons["Sign In"]
+        
+        evaluateElementExist(signinButton)
+        app.textFields["Username"].tap()
+        signIn("nonexistinguser", password: "password")
+        app.buttons["Sign In"].tap()
+        confirmAlert("Error", message: "Not Authorized. Please check your credentials and try again.")
+    }
+    
+    func test2registerExistingUser() {
+        signIn("serveruser", password: "password")
+        app.buttons["Register"].tap()
+        confirmAlert("Error Registering User", message: "You have tried to create a duplicate entry.")
+    }
+    
+    func test3registerEmptyUserName() {
+        signIn("", password: "password")
+        app.buttons["Register"].tap()
+        confirmAlert("Error", message: "Username must be at least 5 characters in length.")
+    }
+    
+    func test4registerEmptyPassword() {
+        signIn("newuser", password: "")
+        app.buttons["Register"].tap()
+        confirmAlert("Error", message: "You must provide a password")
+    }
+    
+    func test5signInEmptyUsername() {
+        signIn("", password: "password")
+        app.buttons["Sign In"].tap()
+        confirmAlert("Error", message: "Username must be at least 5 characters in length.")
+    }
+    
+    func test6signInEmptyPassword() {
+        signIn("newuser", password: "")
+        app.buttons["Sign In"].tap()
+        confirmAlert("Error", message: "You must provide a password")
+    }
+    
+    func test7registerUser() {
+        let findOpponentButton = app.buttons["Find Opponent"]
+        
+        signIn("newuser", password: "password")
+        app.buttons["Register"].tap()
+        evaluateElementExist(findOpponentButton)
+        XCTAssertEqual(app.staticTexts["Connected as newuser"].exists, true)
+    }
+    
+    func test8signInUser() {
+        let findOpponentButton = app.buttons["Find Opponent"]
+        
+        signIn("newuser", password: "password")
+        app.buttons["Sign In"].tap()
+        evaluateElementExist(findOpponentButton)
+        XCTAssertEqual(app.staticTexts["Connected as newuser"].exists, true)
+    }
+}
