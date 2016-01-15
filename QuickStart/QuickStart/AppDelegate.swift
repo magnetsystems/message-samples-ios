6df5362042df6d0c73d1bb6916bf0aca7b0486d2
Lorenzo Stanton <lorenzo.stanton@Lorenzo-Stantons-MacBook-Pro.local>
2016-01-15 11:59:15 -0800

    Updated Licensing

--- QuickStart/QuickStart/AppDelegate.swift
+++ QuickStart/QuickStart/AppDelegate.swift
@@ -1,29 +1,29 @@
 /*
- * Copyright (c) 2015 Magnet Systems, Inc.
- * All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you
- * may not use this file except in compliance with the License. You
- * may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+* Copyright (c) 2015 Magnet Systems, Inc.
+* All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you
+* may not use this file except in compliance with the License. You
+* may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 
 import UIKit
 import MagnetMax
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -43,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
-   // MARK: Push Notification Control
+    // MARK: Push Notification Control
     
     
     func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
@@ -53,8 +53,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         MMDevice.updateCurentDeviceToken(deviceToken, success: { () -> Void in
             print("Successfully updated device token")
-        }, failure:{ (error) -> Void in
-            print("Error updating device token. \(error)")
+            }, failure:{ (error) -> Void in
+                print("Error updating device token. \(error)")
         })
     }
     
