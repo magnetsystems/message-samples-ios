f02dad5631964f0ee9da9b21178b28c69d884c4e
Vladimir Yevdokimov <vevdokimov@geeksforless.net>
2016-03-18 22:25:57 +0200

    source move

---
+++ Messengerv2/ios/ChatKit/YVChatKit.h
@@ -0,0 +1,16 @@
+//
+//  YVChatKit.h
+//  ChatKit
+//
+//  Created by Vladimir Yevdokimov on 3/18/16.
+//  Copyright © 2016 Vladimir Yevdokimov. All rights reserved.
+//
+
+#ifndef YVChatKit_h
+#define YVChatKit_h
+
+#import "CHKUtils.h"
+#import "ChannelCell.h"
+#import "SignIn"
+
+#endif /* YVChatKit_h */
