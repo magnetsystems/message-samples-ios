cc5a91337cbe192460d3405accea7c8273d0bb36
Vladimir Yevdokimov <vevdokimov@geeksforless.net>
2016-03-22 20:22:16 +0200

    Starting code for Messenger v2 app, using Lorenzo Stanton ChatKitUI pod

---
+++ Messengerv2/using_ChatKitUI_pod/Messenger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Messenger
+//
+//  Created by Vladimir Yevdokimov on 3/22/16.
+//  Copyright © 2016 Vladimir Yevdokimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
