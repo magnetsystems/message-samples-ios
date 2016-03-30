275355795633c5ed953343a6415a606997c16e8a
Lorenzo Stanton <lorenzo.stanton@Lorenzo-Stantons-MacBook-Pro.local>
2016-03-29 17:53:23 -0700

    Updated profile image updating

--- messengerv3/Messenger/Messenger/ViewControllers/UserProfileViewController.swift
+++ messengerv3/Messenger/Messenger/ViewControllers/UserProfileViewController.swift
@@ -1,19 +1,19 @@
 /*
-* Copyright (c) 2016 Magnet Systems, Inc.
-* All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you
-* may not use this file except in compliance with the License. You
-* may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-* implied. See the License for the specific language governing
-* permissions and limitations under the License.
-*/
+ * Copyright (c) 2016 Magnet Systems, Inc.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 
 import MagnetMax
@@ -22,7 +22,7 @@ import UIKit
 import ChatKit
 
 class UserProfileViewController: BaseViewController {
-
+    
     
     //MARK: Public properties
     
@@ -31,14 +31,14 @@ class UserProfileViewController: BaseViewController {
     @IBOutlet weak var lastNameTF: UITextField!
     @IBOutlet weak var userAvatarIV: UIImageView!
     @IBOutlet weak var userEmailL: UILabel!
-
+    
     
     //MARK: Overrides
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         userAvatarIV.layer.cornerRadius = userAvatarIV.frame.size.width/2
         userAvatarIV.layer.masksToBounds = true
@@ -59,7 +59,7 @@ class UserProfileViewController: BaseViewController {
             userEmailL.text = user.userName
             
             
-           ChatKit.Utils.loadUserAvatar(user, toImageView: self.userAvatarIV, placeholderImage: UIImage(named: "user_default")!)
+            ChatKit.Utils.loadUserAvatar(user, toImageView: self.userAvatarIV, placeholderImage: UIImage(named: "user_default")!)
         }
     }
     
@@ -81,7 +81,7 @@ class UserProfileViewController: BaseViewController {
             print("updated \(MMUser.currentUser())")
             self?.hideLoadingIndicator()
             self?.showAlert("Changes are saved", title: "Saved", closeTitle: kStr_Close)
-            }) { [weak self] error in
+        }) { [weak self] error in
             print("update err \(error)")
             self?.hideLoadingIndicator()
         }
@@ -108,17 +108,20 @@ extension UserProfileViewController: UIImagePickerControllerDelegate, UINavigati
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-        if let user = MMUser.currentUser() {
-            
-            user.setAvatarWithData(UIImageJPEGRepresentation(pickedImage, 0.1), success: { (url) -> Void in
-                print("avatar updated, new url \(url)")
-                if let url = url where url.absoluteString.characters.count > 0 {
-                    ChatKit.Utils.loadImageWithUrl(url, toImageView: self.userAvatarIV, placeholderImage: nil)
-                }
-                }, failure: { (error) -> Void in
-                    print("avatar update error \(error)")
-            })
-        }
+            if let user = MMUser.currentUser() {
+                
+                user.setAvatarWithData(UIImageJPEGRepresentation(pickedImage, 0.1), success: { (url) -> Void in
+                    print("avatar updated, new url \(url)")
+                    if let url = url where url.absoluteString.characters.count > 0 {
+                        if let path = url.path {
+                            UtilsImageCache.sharedCache.removeObjectForKey(path)
+                        }
+                        ChatKit.Utils.loadImageWithUrl(url, toImageView: self.userAvatarIV, placeholderImage: nil)
+                    }
+                    }, failure: { (error) -> Void in
+                        print("avatar update error \(error)")
+                })
+            }
         }
         
         dismissViewControllerAnimated(true, completion: nil)
